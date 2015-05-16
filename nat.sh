35122e823b659bdb4643e022d50942dabf1b10a2
Shing Chen <shing.chen@cloudtp.com>
2015-05-16 11:38:21 -0500

    Shell script to update default route to a NAT instance

---
+++ nat.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MAC_ADDRESS=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/`
+VPC_ID=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC_ADDRESS:-1}/vpc-id`
+
+INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
+REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+#ROUTE_TABLES=`aws ec2 describe-tags --filter "Name=resource-type,Values=route-table"  "Name=key,Values=network" "Name=value,Values=private" --region $REGION --output text | awk '{print $3}'`
+ROUTE_TABLES=`Shings-Air:aws-auto-healing-nat schen$ aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$VPC_ID" --filters "Name=tag:$TAG_KEY,Values=$TAG_VALUE"`
+
+for i in $ROUTE_TABLES
+do
+   aws ec2 replace-route --route-table-id $i --destination-cidr-block 0.0.0.0/0 --instance-id $INSTANCE_ID --region $REGION
+   aws ec2 create-route --route-table-id $i --destination-cidr-block 0.0.0.0/0 --instance-id $INSTANCE_ID --region $REGION
+done
+
+# disable source destination check
+aws ec2 modify-instance-attribute --instance-id $INSTANCE_ID --no-source-dest-check --region $REGION
